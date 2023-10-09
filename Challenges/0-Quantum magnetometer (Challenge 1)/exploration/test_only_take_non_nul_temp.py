62b16a34b4ab69c873898002e361815f19bb4574
vibqetowi <abutcher.usa@protonmail.com>
2023-10-08 21:30:28 -0400

    doin data cleaning but it's sus

--- Challenges/0-Quantum magnetometer (Challenge 1)/exploration/test_only_take_non_nul_temp.py
+++ Challenges/0-Quantum magnetometer (Challenge 1)/exploration/test_only_take_non_nul_temp.py
@@ -1,3 +1,10 @@
-# test descripton: handle NaN values in time, current and voltage by replacing by 0
-# drop all temperature columns that have even a single NaN, see accuracy
+my_list = [10, 5, 8, 30, 15, 30, 40,40]
 
+# Find the maximum value in the list
+max_value = max(my_list)
+
+# Find the indices of all occurrences of the maximum value
+max_indices = [index for index, value in enumerate(my_list) if value == max_value]
+
+print("Maximum Value:", max_value)
+print("Indices of Maximum Value:", max_indices)
